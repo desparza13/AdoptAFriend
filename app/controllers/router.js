0b01057e0fb5430156d876ac27029475d5f4ad01
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-11-19 20:50:47 -0600

    server y package JSON

---
+++ app/controllers/router.js
@@ -0,0 +1,47 @@
+"use strict"
+
+const express = require('express');
+const router = express.Router();
+
+const adminPet = require('../routes/admin_pet')
+const pet = require('../routes/pet');
+
+const adminAdoptante = require('../routes/admin_adoptante')
+const adoptante = require('../routes/adoptante');
+
+
+const adminRescatista = require('../routes/admin_rescatista')
+const rescatista = require('../routes/rescatista');
+
+//Rutas directas a home y shopping cart
+// router.use(express.static('app/views'));
+// router.use('/home',express.static('./app/views/home.html'));
+// router.use('/shopping_cart',express.static('./app/views/shopping_cart.html'));
+
+// router.use('/admin/products',validateAdmin,adminProductRouter);
+
+router.use('/admin/pet',adminPet);
+router.use('/pet',pet);
+
+router.use('/admin/adoptante',adminAdoptante);
+router.use('/adoptante',adoptante);
+
+router.use('/admin/rescatista',adminRescatista);
+router.use('/rescatista',rescatista);
+
+// const path = require('path');
+
+// router.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname,'..', 'views', 'home.html'));
+//   });
+
+
+// function validateAdmin(req,res,next){   //Autenticador
+//     let adminToken = req.get('x-auth');
+//     if(adminToken == undefined || adminToken!= 'admin'){
+//         return res.status(403).send("Acceso no autorizado, no se cuenta con privilegios de administrador");
+//     }
+//     next();
+// }
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const express = require('express');
+const router = require('./app/controllers/router');
+
+const app = express();
+const port = 3000;
+//const cors = require('cors');
+
+app.use(cors({
+  origin : ['http://127.0.0.1:5500']
+}));
+app.use(express.json()); // Use express body-parser to parse all request bodies.
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+}) 
