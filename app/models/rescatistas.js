85038f2571be08a91a8f142ca1f2084440433ccf
Jennifer Hernández <jennifer.hernandez@iteso.mx>
2022-11-27 19:20:38 -0600

    Schemas

--- app/models/rescatistas.js
+++ app/models/rescatistas.js
@@ -2,6 +2,30 @@
 
 const mongoose = require('mongoose');
 
-let mongoDB = 'mongodb+srv://Jenn:Jenn_123@adoptafriend.sszez2p.mongodb.net/test';
+let mongoDB = 'mongodb+srv://Jenn:Jenn_123@adoptafriend.sszez2p.mongodb.net/AdoptAFriend';
 let options = {useNewUrlParser: true, useUnifiedTopology: true};
 
+mongoose.connect(mongoDB,options);
+
+let rescatistaSchema = mongoose.Schema({
+    nombre:{
+        type: String,
+        required: true
+    },
+    correo:{
+        type: String,
+        required: true
+    },
+    usuario:{
+        type: String,
+        required: true
+    },
+    ciudad:{
+        type: String,
+        required: true
+    }
+})
+
+let Rescatista = mongoose.model('rescatista',rescatistaSchema);
+
+module.exports = Rescatista;
