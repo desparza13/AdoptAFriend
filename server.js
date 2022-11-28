262c333b23bde5e185c7aa4db5e21d94d56729c4
desparza13 <desparzaespinosa@gmail.com>
2022-11-28 11:51:48 -0600

    cors para ajax

--- server.js
+++ server.js
@@ -5,19 +5,14 @@ const router = require('./app/controllers/router');
 
 const app = express();
 const port = 3000;
-//const cors = require('cors');
+const cors = require('cors');
 
-// app.use(cors({
-//   origin : ['http://127.0.0.1:5500']
-// }));
+app.use(cors({
+   origin : ['http://127.0.0.1:5500']
+}));
 app.use(express.json()); // Use express body-parser to parse all request bodies.
 app.use(router);
-app.get('/',
-  (req, res) => res.send('Hello DASWorld!')
-);
-app.route('/home').get(
-  (req, res) => res.send('DASWorld Home')
-);
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`);
 }) 
