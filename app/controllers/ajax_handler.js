40e67b3a264384f0b87d71a8a882c4177a8198eb
desparza13 <desparzaespinosa@gmail.com>
2022-11-29 07:25:17 -0600

    login rescatista conectado al frontend

--- app/controllers/ajax_handler.js
+++ app/controllers/ajax_handler.js
@@ -1,4 +1,20 @@
 "use strict";
+//Carga el adoptante nuevo
+function loadNewAdoptante(url,newAdoptante,onSuccess,onError){
+    let xhr = new XMLHttpRequest(); //Hace el request
+    xhr.open('POST',url)
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(JSON.stringify(newAdoptante));
+    xhr.onload = () => getXhrResponse(xhr,onSuccess,onError);
+}
+//Hacer login rescatista
+function loadLoginRescatista(url,login,onSuccess,onError){
+    let xhr = new XMLHttpRequest(); //Hace el request
+    xhr.open('POST',url)
+    xhr.setRequestHeader('Content-Type','application/json');
+    xhr.send(JSON.stringify(login));
+    xhr.onload = () => getXhrResponse(xhr,onSuccess,onError);
+}
 //Carga el rescatista nuevo
 function loadNewRescatista(url,newRescatista,onSuccess,onError){
     let xhr = new XMLHttpRequest(); //Hace el request
--- app/utils/registroRescatista_utils.js
+++ app/utils/registroRescatista_utils.js
@@ -2,6 +2,7 @@
 
 let noResultsContainer = document.getElementById('noResults');
 const rescatistaUrl = 'http://localhost:3000/admin/rescatista'
+const loginRescatistaUrl = 'http://localhost:3000/login/rescatista'
 
 function uploadRescatista(){
     let usuario = document.getElementById('usuario').value;
@@ -20,5 +21,15 @@ function uploadRescatista(){
         console.log(rescatista);
     },(error)=>console.log(error));
 }
-
+function loginRescatista(){
+    let loginCorreo = document.getElementById('email').value;
+    let loginPassword = document.getElementById('password').value;
+    let newLogin = new Object();
+    newLogin.correo = loginCorreo;
+    newLogin.password = loginPassword;
+    console.log(JSON.stringify(newLogin));
+    loadLoginRescatista(loginRescatistaUrl,newLogin, login=>{
+        console.log(newLogin);
+    },(error)=>console.log(error));
+}
 
