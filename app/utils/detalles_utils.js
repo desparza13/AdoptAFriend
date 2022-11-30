49aff9205b24b5012f3d8f5e5943f841657f1579
desparza13 <desparzaespinosa@gmail.com>
2022-11-29 21:14:27 -0600

    mostrar detalles de mascota, su rescatista y perronalidad en Adoptantes y en Rescatistas

--- app/utils/detalles_utils.js
+++ app/utils/detalles_utils.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const petsUrl = 'http://localhost:3000/pet'
+const rescatistaUrl = 'http://localhost:3000/rescatista/';
 //Cargar los detalles de una mascota
 function loadPetDetails(){
     let pet = sessionStorage.getItem('petDetails');
@@ -8,7 +9,11 @@ function loadPetDetails(){
     loadPet(petsUrl+'/'+pet).then(petDetail =>{
         console.log(JSON.stringify(petDetail))
         writePetDetails(petDetail);
+        loadRescatista(rescatistaUrl+petDetail.idRescatista).then(rescatista =>{
+            writeRescatistaDetails(rescatista);
+        });
     });
+
 };
 function writePetDetails(petDetail){
     //Mascota
@@ -21,6 +26,8 @@ function writePetDetails(petDetail){
     let edadDetalles = document.getElementById("edadDetalles");
     let tallaDetalles = document.getElementById("tallaDetalles");
     let ciudadDetalles = document.getElementById("ciudadDetalles");
+    let perronalidad = document.getElementById("perronalidad");
+    let perronalidadDetalles = document.getElementById("perronalidadDetalles");
     nombreDetalles.innerText = petDetail.nombre;
     imgDetalles.src = petDetail.petImg;
     nombreMascota.innerText = petDetail.nombre;
@@ -30,14 +37,35 @@ function writePetDetails(petDetail){
     edadDetalles.innerText = petDetail.edad;
     tallaDetalles.innerText = petDetail.talla;
     ciudadDetalles.innerText = petDetail.ciudad;
-    //---------------------------------------------------------------
+    perronalidad.innerText = petDetail.perronalidad;
+    perronalidadDetalles.innerText = perronalidadDetails(petDetail.perronalidad);
+}
+function writeRescatistaDetails(rescatista){
     //Rescatista
     let nombreRescatista = document.getElementById("nombreRescatista");
     let correoRescatista = document.getElementById("correoRescatista");
     let usuarioRescatista = document.getElementById("usuarioRescatista");
-    let perronalidadRescatista = document.getElementById("perronalidadRescatista");
-    
-
-
+    nombreRescatista.innerText = rescatista.nombre;
+    correoRescatista.innerText = rescatista.correo;
+    usuarioRescatista.innerText = '@'+rescatista.usuario;
+}
+function perronalidadDetails(perronalidad){
+    if(perronalidad=='Fiestero'){
+        return 'Pienso que todo es divertido, interesante y hecho para jugar, especialmente tú. Todo lo que hagas, yo también lo querré hacer. Con mi propia clase de sorpresas, vivir conmigo te mantendrá constantemente de pie. Diversión garantizada.';
+    }else if(perronalidad=='Independiente'){
+        return 'Inteligente, independiente, ingenioso y de confianza, prefiero tomar mis propias decisiones pero te escucharé si tienes buenos argumentos. Somos compañeros en esta aventura. Trátame como tal y ambos viviremos felices para siempre.';
+    }else if(perronalidad=='Dinámico'){
+        return '¿Quieres hacer más ejercicio? Acción es mi segundo nombre. Mi estilo de vida te mantendrá motivado a salir de la casa y moverte. Mi lema es “VAMOS!”. Tengo toneladas de energía, y tal como el sol, estoy quemando calorías y trabajando 24 horas al día, los 7 días de la semana. Correré kilómetros, perseguiré la pelota durante horas y, aún así, querré jugar al final del día.';
+    }else if(perronalidad=='Tímido'){
+        return 'Perro tímido y encantador busca dueño paciente con un estilo de vida relajado. Busco alguien que me ayude a salir de mi cascarón de manera gentil. Trátame de manera dulce y amable, y yo me desarrollaré bien.';
+    }else if(perronalidad=='Juguetón'){
+        return 'Soy un perro naturalmente juguetón, curioso y de confianza. Llévame a una caminata larga todos los días, dame algo que hacer. Después de que mi trabajo está hecho, me acurrucaré contigo frente al fuego en las tardes.';
+    }else if(perronalidad=='Faldero'){
+        return '¿Buscas una relación emocionalmente segura, mutuamente satisfactoria y de poco mantenimiento? Yo soy lo que necesitas. Permíteme sentarme a tus pies, caminar a tu lado, y siempre seré tu devoto compañero.';
+    }else if(perronalidad=='Tapete'){
+        return '¿Te gusta la vida fácil? Entonces yo soy el compañero ideal para ti. Soy de esos perros relajados y despreocupados, que disfruta de largas siestas, ver películas, acurrucarse en el regazo, y caminar distancias muy cortas del sofá al envase de comida y viceversa.';
+    }else if(perronalidad=='Inteligente'){
+        return 'Yo tengo todo lo que buscas: soy inteligente y peludo, tengo 4 patas, amo aprender y vivo para complacer. Adelante, enséñame lo que quieras. Sentarse, quedarse quieto, balancear tu chequera, yo puedo hacerlo todo. Mantenme entretenido y seré tuyo para siempre.';
+    }
 }
 loadPetDetails();
